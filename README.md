d8176f37d96cb061f3b4f6428ac454a60334dc1f
Phil Cole <filcole@gmail.com>
2022-08-14 01:01:05 +0100

    v 1.11.7

--- README.md
+++ README.md
@@ -49,3 +49,20 @@ Plugin:
 
 * plugin.csproj: Added reference to ProjectTypeGuids 4C25E9B5-9FA6-436c-8E19-B395D2A65FAF and FAE04EC0-301F-11D3-BF4B-00C04F79EFBC. (CWA and C# I think)
 * Add comments throughout and alter Trace() function (buggy)
+
+## 1.11.7
+
+PCF:
+
+Plugin:
+
+Package:
+
+* Remove empty data_schema.xml and data.xml files
+* Clear data.zip from ImportConfig.xml
+* Remove reference to CSD from AssemblyInfo.cs
+* Remove CDS from filenames of CDSPackage.sln, CDSpackage.csproj, and CDSPackageImportExtensions.cs
+
+Plugin:
+
+* Plugin1.cs: Use `nameof()` and `var` instead of explicit types
--- package/Properties/AssemblyInfo.cs
+++ package/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CDSPackage")]
+[assembly: AssemblyTitle("package")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CDSPackage")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyProduct("package")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
